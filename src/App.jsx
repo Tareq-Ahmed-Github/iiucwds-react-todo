2d41916a8bf2510feb4736cc6167a223575a8f1c
Tareq Ahmed <tareqrobin57@gmail.com>
2023-12-02 21:34:45 +0600

    logo added,made todo list responsive

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,8 @@ function App() {
   return (
     <>
       <div className="w-3/4 m-4 p-4 mx-auto flex flex-col gap-2 items-center bg-slate-300 rounded">
+        <img src="assets/completed_tasks.svg" alt="todo" className="h-48"/>
+        <h1 className="text-2xl font-semibold text-gray-700 font-sans">Osthir Todo Pro</h1>
         <input
           className="border rounded py-1 px-2 outline-none border-1 border-gray-400"
           type="text"
@@ -37,17 +39,17 @@ function App() {
           value={message}
         />
         <button
-          className="px-3 py-2 bg-gray-800 rounded text-white"
+          className="px-3 py-2 bg-gray-800 rounded text-white font-sans"
           onClick={todoObjects}
         >
           Add
         </button>
-        <div className="grid grid-cols-3 gap-2">
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2">
           {todoList &&
             todoList.map((todo) => {
               return (
                 <div
-                  className=" border border-gray-400 rounded p-4 m-4 text-center"
+                  className=" border border-gray-400 rounded p-4 m-4 text-center w-52"
                   key={todo.id}
                 >
                   <p className="text-2xl font-semibold text-gray-900 mb-2">
