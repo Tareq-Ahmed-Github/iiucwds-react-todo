69612eb03b07003c5b643009d87077c475cbdce9
Tareq Ahmed <tareqrobin57@gmail.com>
2023-12-02 19:51:09 +0600

    vite react tailwind setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <h1 className="text-red-600">hello sir</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
