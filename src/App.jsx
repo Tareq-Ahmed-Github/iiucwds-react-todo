1d22bb185e617e006c72332087ef85651e22bb18
Tareq Ahmed <tareqrobin57@gmail.com>
2023-12-02 20:12:34 +0600

    input div created

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="container mx-auto">
-        <h1 className="text-red-600">hello sir</h1>
+      <div className="w-3/4 m-4 p-4 mx-auto flex flex-col gap-2 items-center bg-slate-300 min-h-screen">
+        <input className="border rounded py-1 px-2 outline-none border-1 border-gray-400" type="text" />
+        <button className="px-3 py-2 bg-gray-800 rounded text-white">Add</button>
       </div>
     </>
   );
